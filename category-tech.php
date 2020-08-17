d58dc16d6f47d9d852ca0d2df59982dc2e9b4608
shamsad-anam <reeddhy@gmail.com>
2020-08-18 03:13:28 +0600

    added another category

---
+++ category-tech.php
@@ -0,0 +1,18 @@
+<?php get_header(""); ?>
+<div class="container">
+    <h5 class="text-muted">From Category Tech Template</h5>
+    <h6 class="bg-warning my-2 p-2">
+        Category : Tech
+    </h6>
+    <div class="row">
+        <!-- this template part contains the loop  -->
+        <?php get_template_part("includes/section", "archive"); ?>
+    </div>
+
+    <!-- the pagination - previous and next -->
+    <div class="text-right py-2">
+        <?php previous_posts_link(); ?>
+        <?php next_posts_link(); ?>
+    </div>
+</div>
+<?php get_footer(); ?>
